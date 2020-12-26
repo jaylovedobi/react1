11d13d8da59bfc7281319b54bf0cb368a9020fd9
unknown <tuyishimejeand88@gmail.com>
2020-12-26 17:15:34 +0200

    first commit tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+} from 'reactstrap';
+import Menucomponent from './components/MenuComponent'
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar color="primary" dark>
+       <div className="container">
+         <NavbarBrand href='/'>restraunt con funsion</NavbarBrand>
+       </div>
+      </Navbar>
+      <Menucomponent />
     </div>
-  );
+  )
+   }
 }
 
 export default App;
